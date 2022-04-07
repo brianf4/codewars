fd04b56d29be5b2856340970095059b1b4150693
Brian Fuentes <Brian.Fuentes04@gmail.com>
2022-04-07 10:45:12 -0700

    added solution to problem

--- 8kyu/lario-muigi-problem/main.js
+++ 8kyu/lario-muigi-problem/main.js
@@ -1,1 +1,10 @@
-// test test!
+function pipeFix(numbers){
+  let first = numbers[0];
+  let last = numbers[numbers.length-1];
+  
+  let arr = [];
+  for(let i = first; i <= last; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
