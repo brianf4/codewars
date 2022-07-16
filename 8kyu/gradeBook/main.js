aa09fe543bfc0f15fc64daa02211eccfbbd37c53
Brian Fuentes <Brian.Fuentes04@gmail.com>
2022-07-15 20:05:29 -0700

    added solution

---
+++ 8kyu/gradeBook/main.js
@@ -0,0 +1,16 @@
+function getGrade (s1, s2, s3) {
+  let average = (s1 + s2 + s3) / 3
+  
+  if (average <= 100 && average >= 90) {
+    return 'A';
+  } else if (average < 90 && average >= 80) {
+    return 'B';
+  } else if (average < 80 && average >= 70) {
+    return 'C';
+  } else if (average < 70 && average >= 60) {
+    return 'D';
+  } else if (average < 60 && average >= 0) {
+    return 'F';
+  }
+  
+}
