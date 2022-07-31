a23e2a128253ce1ddb287ea2e9a0bc2026968f32
Brian Fuentes <brian.fuentes04@gmail.com>
2022-07-31 13:25:32 -0700

    added solution

---
+++ 8kyu/sumOfDifferencesInArray/main.js
@@ -0,0 +1,17 @@
+function sumOfDifferences(arr) {
+    if (arr.length < 2) {
+      return 0
+    }
+     
+     let total = 0;
+     let newArr = arr.sort((a,b) => b - a)
+     
+     for (let i = 0; i < newArr.length; i++){
+       if (i === newArr.length - 1){
+           break;
+       }else {
+           total += (newArr[i] - newArr[i + 1])
+       }
+     }
+     return total
+   }
